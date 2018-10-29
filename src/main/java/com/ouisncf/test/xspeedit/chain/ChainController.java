822bd8d2044be563b61f21eaf8475a6b015b5487
Nicolas Gaye <nicolas.gaye@gmail.com>
2018-10-29 19:47:55 +0100

    Specifie les types des reponse et de requetes du ChainController

--- src/main/java/com/ouisncf/test/xspeedit/chain/ChainController.java
+++ src/main/java/com/ouisncf/test/xspeedit/chain/ChainController.java
@@ -1,12 +1,19 @@
 package com.ouisncf.test.xspeedit.chain;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("chains")
+@RequestMapping(
+        value = "chains",
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+)
 public class ChainController {
 
     private ChainService chainService;
